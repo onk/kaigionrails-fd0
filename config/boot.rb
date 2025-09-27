b6cb9c4cf35229a7c7a6978529e6caf1d268cfc2
Takafumi ONAKA <takafumi.onaka@gmail.com>
2025-09-27 09:56:14 +0900

    bin/rails s で起動するように

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,12 @@
-ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
+# Set up gems from vendor/bundle without Bundler
+gem_path = File.expand_path("../vendor/bundle/ruby/3.4.0/gems", __dir__)
+Dir.glob("#{gem_path}/*").sort.each do |gem_dir|
+  lib_path = "#{gem_dir}/lib"
+  $LOAD_PATH.unshift(lib_path) if File.directory?(lib_path)
 
-require "bundler/setup" # Set up gems listed in the Gemfile.
+  # Add concurrent-ruby's nested structure to load path
+  if gem_dir.include?("concurrent-ruby")
+    concurrent_lib = "#{gem_dir}/lib/concurrent-ruby"
+    $LOAD_PATH.unshift(concurrent_lib) if File.directory?(concurrent_lib)
+  end
+end
