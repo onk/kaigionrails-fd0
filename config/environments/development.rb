0ac2364de4256a12357987b02bdca38ac576f366
Takafumi ONAKA <takafumi.onaka@gmail.com>
2025-09-27 09:49:30 +0900

    ActionDispatch::ServerTiming も無効化

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   config.consider_all_requests_local = true
 
   # Enable server timing.
-  config.server_timing = true
+  config.server_timing = false
 
   # Enable/disable Action Controller caching. By default Action Controller caching is disabled.
   # Run rails dev:cache to toggle Action Controller caching.
