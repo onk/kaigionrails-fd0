5999fdf0fd18f738a6a51decbf49af2aad0c69ee
Takafumi ONAKA <takafumi.onaka@gmail.com>
2025-09-27 11:07:12 +0900

    concurrent

--- config/boot.rb
+++ config/boot.rb
@@ -3,10 +3,4 @@ gem_path = File.expand_path("../vendor/bundle/ruby/3.4.0/gems", __dir__)
 Dir.glob("#{gem_path}/*").sort.each do |gem_dir|
   lib_path = "#{gem_dir}/lib"
   $LOAD_PATH.unshift(lib_path) if File.directory?(lib_path)
-
-  # Add concurrent-ruby's nested structure to load path
-  if gem_dir.include?("concurrent-ruby")
-    concurrent_lib = "#{gem_dir}/lib/concurrent-ruby"
-    $LOAD_PATH.unshift(concurrent_lib) if File.directory?(concurrent_lib)
-  end
 end
