fb3d10ca6ec7d1caa072464b4aed1a8a0427f608
Takafumi ONAKA <takafumi.onaka@gmail.com>
2025-09-27 14:40:21 +0900

    tar コマンド無くても動くように

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,7 @@
 # Set up gems from vendor_bundle.tgz without Bundler
 require 'tmpdir'
+require 'zlib'
+require 'rubygems/package'
 
 vendor_bundle_path = File.expand_path("../vendor_bundle.tgz", __dir__)
 
@@ -7,8 +9,25 @@ if File.exist?(vendor_bundle_path)
   # Create temporary directory for extracting the bundle
   temp_dir = Dir.mktmpdir("vendor_bundle_")
 
-  # Extract vendor_bundle.tgz to temporary directory
-  system("tar -xzf #{vendor_bundle_path} -C #{temp_dir}")
+  # Extract vendor_bundle.tgz to temporary directory using Ruby
+  File.open(vendor_bundle_path, 'rb') do |file|
+    Zlib::GzipReader.wrap(file) do |gz|
+      Gem::Package::TarReader.new(gz) do |tar|
+        tar.each do |entry|
+          if entry.directory?
+            FileUtils.mkdir_p(File.join(temp_dir, entry.full_name))
+          elsif entry.file?
+            file_path = File.join(temp_dir, entry.full_name)
+            FileUtils.mkdir_p(File.dirname(file_path))
+            File.open(file_path, 'wb') do |f|
+              f.write(entry.read)
+            end
+            File.chmod(entry.header.mode, file_path) if entry.header.mode
+          end
+        end
+      end
+    end
+  end
 
   # Set up gem paths from extracted bundle
   gem_path = File.join(temp_dir, "vendor/bundle/ruby/3.4.0/gems")
